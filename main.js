8db0c20e7866153a67876179f257fc46b997e997
TARUNPREET <tarunsuri2697@gmail.com>
2018-10-05 06:56:14 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+window.onload=function(){
+    document.getElementById('open').addEventListener('click',function()
+    {
+        document.querySelector(".bg-modal").style.display='flex';
+    });
+    document.getElementById('close').addEventListener('click',function()
+    {
+        document.querySelector(".bg-modal").style.display='none';
+    });
+  }
