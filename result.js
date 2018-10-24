4919a1217d79aabbb133b7b3108862bbae050528
TARUNPREET <tarunsuri2697@gmail.com>
2018-10-24 09:28:23 +0530

    updated

--- result.js
+++ result.js
@@ -2,47 +2,35 @@ window.onload = function runonstart()
 {
     var bjp=takevalue_bjp();
     var cong=takevalue_cong();
-    console.log(bjp);
-    // var winner;
-    // if (bjp>cong)
-    // {
-    //   winner="BJP";
-    // }
-    // else
-    // {
-    //     winner="CONGRESS";
-    // }
-    // let output="<br><br><h4>Result</h4><br>";
-    // output+=`
-    // <h5>${ winner} is winning at this moment....</h5> 
-    //  `
-    //  document.getElementById('declare').innerHTML=output;
-}
+    
+}    
 function takevalue_bjp()
 {
-   var b;
-    fetch('https://opinion-2019.herokuapp.com/bjp')
+    var b=fetch('https://opinion-2019.herokuapp.com/bjp')
     .then((res)=> res.json())
     .then((data)=>{
         let output="<h4>BJP</h4><br>";
         console.log(data)
-        const arr = Object.keys(data).map(function(_) { return data[_]; })
+        arr = Object.keys(data).map(function(_) { return data[_]; })
         arr[3]=arr[3].toFixed(2);
         arr[4]=arr[4].toFixed(2);
-        b=arr[3];
+        
             output+=`
                <ul>
-                  <li>Positive Percentage: ${ arr[3] + '%'}</li>
-                  <li>Negative Percentage: ${arr[4] + '%'}</li>
+                  
                   <li>Positive Reviews: ${ arr[1]}</li>
+                  <br>
                   <li>Negative Reviews: ${ arr[2]}</li>
+                  <br>
+                  <li>Total Reviews: ${ arr[1]+arr[2]}</li>
+                  <br>
                   <li>Opinion: ${arr[0]}</li>
                 </ul>
                 `
     
         document.getElementById('bjp').innerHTML=output;
+      
     })
-   return b;
 }
 function takevalue_cong()
 {
@@ -58,20 +46,57 @@ function takevalue_cong()
         c=arra[3];
             output+=`
                <ul>
-                  <li>Positive Percentage: ${ arra[3]+ '%'}</li>
-                  <li>Negative Percentage: ${arra[4]+ '%'}</li>
+                  
                   <li>Positive Reviews: ${ arra[1]}</li>
+                  <br>
                   <li>Negative Reviews: ${ arra[2]}</li>
+                  <br>
+                  <li>Total Reviews: ${ arra[1]+arra[2]}</li>
+                  <br>
                   <li>Opinion: ${arra[0]}</li>
                 </ul>
                 `
-            let changing=`<h3>* Results are continously changing......</h3>
-            <h3>* The Final Result will be available on 10 JANUARY 2019</h3>`    
+        //     let changing=`<h3>* Results are continously changing......</h3>
+        //     <h3>* The Final Result will be available on 10 JANUARY 2019</h3>`    
     
         document.getElementById('congress').innerHTML=output;
-        document.getElementById('change').innerHTML=changing;
+        document.getElementById('results').style.display='inline';
     })
     // return c;
     // console.log(c);
 
+}
+function results()
+{
+    document.querySelector(".bg-modal").style.display='flex';
+    document.getElementById('close').addEventListener('click',function()
+    {
+        document.querySelector(".bg-modal").style.display='none';
+        document.getElementById('rahul').style.display='none';
+        document.getElementById('modi').style.display='none';
+        document.getElementById('who_won').innerHTML='';
+    });
+    fetch('https://opinion-2019.herokuapp.com/comp')
+    .then((res)=> res.json())
+    .then((data)=>{
+        let output="<h4>CONGRESS</h4><br>";
+        console.log(data)
+        var arr1 = Object.keys(data).map(function(_) { return data[_]; })
+        winner=arr1[0]
+          if(winner==="Congress")
+          {
+            document.getElementById('rahul').style.display='inline';
+           
+          }
+          if(winner==="BJP")
+          {
+            document.getElementById('modi').style.display='inline';
+           
+          }
+
+         changing=`<h2>${winner} is Winning Till Now<h2><br>
+         <h3>* Results are continously changing......</h3>
+        <h3>* The Final Result will be available on 10 JANUARY 2019</h3>`
+        document.getElementById('who_won').innerHTML=changing;
+    })
 }
